0e530cbd3b4f9002f3dec267d6f146192c04f60e
Fernando Brito <fernandobs.sp@gmail.com>
2025-02-21 11:19:51 -0300

    Incluindo funcionalidades

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,34 @@
 
+let amigos = []
+
+function adicionarAmigo () {
+    let inputAmigo = document.getElementById("amigo");
+    let nome = inputAmigo.value.trim();
+    if (nome !== "") {
+        amigos.push(nome); 
+        atualizarLista ();
+    } 
+    inputAmigo.value = ""; 
+    if (nome == "") {
+        alert("Digite um nome válido");
+    }
+}  
+function atualizarLista () {
+    let listaAmigos = document.getElementById ("listaAmigos");
+    listaAmigos.innerHTML = ""; 
+    amigos.forEach(function(amigo) { 
+        const li = document.createElement("li"); 
+        li.textContent = amigo; 
+        listaAmigos.appendChild(li);
+    })
+} 
+
+function sortearAmigo () {
+    if (amigos.length === 0) {
+        document.getElementById("resultado").textContent = "Adicione amigos para sortear";
+        return
+    }
+let indiceSorteado = Math.floor(Math.random()*amigos.length);
+let amigoSorteado = amigos[indiceSorteado]; 
+    document.getElementById("resultado").textContent = `O amigo sorteado é ${amigoSorteado}`;
+} 
